436d8d69de62795b3f500dc4570fa7b30d2609fe
ZHU Xinyu <xzhuah@connect.ust.hk>
2016-10-06 02:35:34 +0800

    Hellowolrd

---
+++ Helloworld/src/com/hkust/xinyu/Main.java
@@ -0,0 +1,11 @@
+package com.hkust.xinyu;
+
+/**
+ * Created by xzhua on 10/6/2016.
+ */
+public class Main {
+
+    public static void main(String args[]){
+        System.out.println("Helloworld");
+    }
+}
